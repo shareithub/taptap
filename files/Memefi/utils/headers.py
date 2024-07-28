5b25d5c3c652be857b5f1832326ea26b08065709
LFG-AAI <156384686+LFG-AAI@users.noreply.github.com>
2024-07-27 21:27:56 -0700

    Upload headers.py

---
+++ files/Memefi/utils/headers.py
@@ -0,0 +1,14 @@
+headers_set = {
+  'accept': '*/*',
+  'accept-language': 'en-US,en;q=0.9',
+  'cache-control': 'no-cache',
+  'content-type': 'application/json',
+  'origin': 'https://tg-app.memefi.club',
+  'pragma': 'no-cache',
+  'priority': 'u=1, i',
+  'referer': 'https://tg-app.memefi.club/',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'user-agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11AG Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/125.0.6422.165 Mobile'
+}
